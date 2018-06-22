bd653f05d490099fc470b5cd6e77a4560af433a0
pry <pry@localhost.localdomain>
2018-06-22 08:35:04 +0000

    ..

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config(object):
     DEBUG = False
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    APP_DIR = os.path.abspath(os.path.dirname(__file__))
-    PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+    # APP_DIR = os.path.abspath(os.path.dirname(__file__))
+    # PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
 
 
 
